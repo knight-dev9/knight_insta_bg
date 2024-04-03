4a1a4f8692483165c5711f9772fa295295489447
knight-dev9 <knight.developer9@gmail.com>
2024-04-03 23:48:26 +0530

    Added Unit Test

--- test/insta_bg_test.dart
+++ test/insta_bg_test.dart
@@ -1,7 +1,19 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:insta_bg/insta_bg.dart';
 
 void main() {
-  test('adds one to input values', () {
-
+  test('Testing Internally', () {
+    final bg = InstaBG(primaryColors: const [
+      Color(0xFFFD746C),
+      Color(0xFFFF9068)
+    ], secondaryColors: const [
+      Color(0xFFF15F79),
+      Color(0xFFF15F79)
+    ]);
+    assert(bg != null);
+    assert(bg is Widget);
   });
 }
